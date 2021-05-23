423b3efaf73d4c46302d3957cb81a7c3c01cd8c1
YunKukPark <a01043340999@gmail.com>
2021-05-23 18:07:39 +0900

    ✏️  do homework

--- javascript/array-homework.js
+++ javascript/array-homework.js
@@ -1,3 +1,4 @@
+'use strict';
 // Q1. make a string out of an array
 {
   const fruits = ['apple', 'banana', 'orange'];
@@ -5,6 +6,9 @@
   //answer :
   const answer = fruits.toString();
   console.log(answer);
+
+  const answer2 = fruits.join(',');
+  console.log(answer2);
 }
 
 // Q2. make an array out of a string
@@ -13,18 +17,37 @@
 
   //answer :
   const answer = fruits.split(',');
+
   console.log(answer);
 }
 
 // Q3. make this array look like this: [5, 4, 3, 2, 1]
 {
   const array = [1, 2, 3, 4, 5];
+
+  // answer :
+  const answer = array.sort((a, b) => b - a);
+
+  console.log(answer);
 }
 
 // Q4. make new array without the first two elements
 {
   const array = [1, 2, 3, 4, 5];
+
+  const answer = [];
+
+  // answer :
+  array.forEach((num) => {
+    const tmp = answer.push(num);
+  });
+
+  const tmp = answer.splice(0, 2);
+
+  console.log(answer);
 }
+
+console.clear();
 
 class Student {
   constructor(name, age, enrolled, score) {
@@ -44,31 +67,60 @@ const students = [
 
 // Q5. find a student with the score 90
 {
+  const answer = students.find((student) => students.score === 90);
+  console.log(answer);
 }
 
 // Q6. make an array of enrolled students
 {
+  const answer = students.filter((student) => student.enrolled);
+  console.log(answer);
 }
 
 // Q7. make an array containing only the students' scores
 // result should be: [45, 80, 90, 66, 88]
 {
+  const answer = students.map((student) => students.score);
+
+  console.log(answer);
 }
 
 // Q8. check if there is a student with the score lower than 50
 {
+  const answer = students.filter((student) => student.score < 50);
+
+  console.log(answer);
 }
 
 // Q9. compute students' average score
 {
+  const scoreArray = students.map((student) => student.score);
+
+  const totalScore = scoreArray.reduce((sum, currValue) => sum + currValue);
+
+  const averageScore = totalScore / students.length;
+
+  console.log(averageScore);
 }
 
 // Q10. make a string containing all the scores
 // result should be: '45, 80, 90, 66, 88'
 {
+  const scoreArray = students.map((student) => student.score);
+
+  const answer = scoreArray.toString();
+
+  console.log(answer);
 }
 
 // Bonus! do Q10 sorted in ascending order
 // result should be: '45, 66, 80, 88, 90'
 {
+  const scoreArray = students.map((student) => student.score);
+
+  const sortedScoreArray = scoreArray.sort((a, b) => a - b);
+
+  const answer = sortedScoreArray.toString();
+
+  console.log(answer);
 }
