4e04cde80265d23a9026d32b825419fb767db001
YunKukPark <a01043340999@gmail.com>
2021-12-03 20:43:26 +0900

    ♻️  refactor repl 30.js

--- javascript/weCode/javascriptRepl/30.js
+++ javascript/weCode/javascriptRepl/30.js
@@ -5,7 +5,6 @@ const inputScoreObject = {
   기초수학: 'D+',
   영어회화: 'C+',
   인지발달심리학: 'A+',
-  ㅋㅋ루삥뽕: 'I',
 };
 
 const inputRequiredClasses = [
@@ -17,58 +16,34 @@ const inputRequiredClasses = [
 
 // Assignment - 다음 함수 안에 코드를 구현하세요
 const getExamResult = (scores, requiredClasses) => {
-  const examScoreTable = Object.entries(scores);
+  const scoreTable = changeGradeScore(scores);
 
-  const convertedNumberTable = examScoreTable.map((scoreRow) => {
-    return [scoreRow[0], translateGrade_2_Score(scoreRow[1])];
+  requiredClasses.map((requiredClass) => {
+    const hasClass = scoreTable[requiredClass];
+    if (!hasClass) scoreTable[requiredClass] = 0;
   });
 
-  let scoreObject = table2object(convertedNumberTable);
-
-  requiredClasses.forEach((requiredClass) => {
-    if (!scoreObject[requiredClass]) {
-      scoreObject[requiredClass] = 0;
-    }
-  });
-
-  return scoreObject;
+  return scoreTable;
 };
 
-const translateGrade_2_Score = (grade) => {
-  switch (grade) {
-    case 'A+':
-      return 4.5;
-    case 'A':
-      return 4;
-    case 'B+':
-      return 3.5;
-    case 'B':
-      return 3;
-    case 'C+':
-      return 2.5;
-    case 'C':
-      return 2;
-    case 'D+':
-      return 1.5;
-    case 'D':
-      return 1;
-    case 'F':
-      return 0;
-    default:
-      return console.error(`성적에 ${grade}라는 성적은 없습니다`);
+const changeGradeScore = (scores) => {
+  const scoreTable = {
+    'A+': 4.5,
+    A: 4,
+    'B+': 3.5,
+    B: 3,
+    'C+': 2.5,
+    C: 2,
+    'D+': 1.5,
+    D: 1,
+    F: 0,
+  };
+
+  for (examClass in scores) {
+    scores[examClass] = scoreTable[scores[examClass]];
   }
-};
 
-const table2object = (table) => {
-  return table
-    .map((row) => {
-      const [key, value] = row;
-      return { key, value };
-    })
-    .reduce((result, item) => {
-      result[item.key] = item.value;
-      return result;
-    }, {});
+  return scores;
 };
 
 console.log(getExamResult(inputScoreObject, inputRequiredClasses));
